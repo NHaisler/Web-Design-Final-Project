8fd9e6491fd657a862fa1b9e88150f4837842728
Nicholas Haisler <nicholas.haisler@drake.edu>
2025-12-12 01:36:47 -0600

    Ready for deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? "https://web-design-final-project.onrender.com" 
+  ? 'https://web-design-final-project.onrender.com' 
   : 'http://localhost:5000';
 
 export default API_BASE_URL;
